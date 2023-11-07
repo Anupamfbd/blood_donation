b23ada9b9fa043f6b6b4449f613c02792bb78ed0
Anupam Yadav <95772605+Anupamfbd@users.noreply.github.com>
2023-11-08 01:50:54 +0530

    Add files via upload
    1.Install XAMPP or WAMPP.
    
    2.Open XAMPP Control panal and start [apache] and [mysql] .
    
    3.Download project from github(https://github.com/varunsardana004/Blood-Bank-And-Donation-Management-System.git).
    
    OR follow gitbash commands
    
        i>cd C:\\xampp\htdocs\
       ii>git clone https://github.com/varunsardana004/Blood-Bank-And-Donation-Management-System.git
    
    4.Extract files in C:\xampp\htdocs.
    
    5.Open link localhost/phpmyadmin
    
    6.Click on new at side navbar.
    
    7.Give a database name as (blood_bank_database) hit on create button.
    
    8.After creating database name click on import.
    
    9.Browse the file in directory[BDMS/sql/blood_bank_database.sql].
    
    10.After importing successfully.
    
    Open any browser and type http://localhost/BDMS/home.php to open User layout.

---
+++ BloodDonation/footer.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+  <style>
+  #footer {
+
+  position:absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 70px;
+  padding-top:10px;
+  word-spacing:5px;
+  background-color:#000000;
+  color:white;
+  text-align: center;
+}
+  </style>
+</head>
+<body>
+  <div id="footer" >
+  <center>Copyright© 2023
+  <br>
+  All Rights Reserved.
+  </center>
+  </div>
+
+</body>
+
+</html>
